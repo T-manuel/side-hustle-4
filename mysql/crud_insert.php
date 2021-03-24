2e9719d74c8d15d75294e6c2860416b1d1f02729
Emmanuel <26921479+T-manuel@users.noreply.github.com>
2021-03-24 07:25:32 +0100

    Update crud_insert.php

--- mysql/crud_insert.php
+++ mysql/crud_insert.php
@@ -21,69 +21,69 @@
     
         if ($email == $confirmEmail){
 			
-			// Create a query for the database to prevent double usernames
-			$c_query = "SELECT * FROM tmanuel_users WHERE usernames = '$name'";
-			$d_query = "SELECT * FROM tmanuel_users WHERE emails = '$email'";
+		// Create a query for the database to prevent double usernames
+		$c_query = "SELECT * FROM tmanuel_users WHERE usernames = '$name'";
+		$d_query = "SELECT * FROM tmanuel_users WHERE emails = '$email'";
 
-			/* Get a response from the database by sending the connection
-			and the query */
+		/* Get a response from the database by sending the connection
+		and the query */
 
-			$result_name_c = @mysqli_query($conn, $c_query);
-			$result_name_d = @mysqli_query($conn, $d_query);
+		$result_name_c = @mysqli_query($conn, $c_query);
+		$result_name_d = @mysqli_query($conn, $d_query);
 
-			//this section checks that the user's mail and username is unique
-			/*if ((mysqli_num_rows($result_name_c) && mysqli_num_rows($result_name_d)) == 0) {
+		//this section checks that the user's mail and username is unique
 
-				$conn->query("INSERT INTO tmanuel_users (usernames, passwords, emails, surname, firstname, middleName) VALUES ('$name',
-				'$passID', '$email', '$surname', '$firstname', '$middleName')") or die("unable to connect to MySQL at the moment ".$conn->connect_error);
+		if ((mysqli_num_rows($result_name_c) && mysqli_num_rows($result_name_d)) == 1){
 
-				unset ($_SESSION["usernames-a"]);
-				unset ($_SESSION["surname-a"]);
-				unset ($_SESSION["firstname-a"]);
-				unset ($_SESSION["emails-a"]);
-				unset ($_SESSION["message"]);
-
-				/*session_start();
-				$_SESSION["usernames"] = $name;
-				$_SESSION["passwords"] = $passID;
-				$_SESSION["emails"] = $email;
-
-				header("location: ../login.php");
-			}*/
-			if ((mysqli_num_rows($result_name_c) && mysqli_num_rows($result_name_d)) == 1){
-
-				$_SESSION['message'] = "username and email exists";
+			$_SESSION['message'] = "username and email exists";
+			$_SESSION["usernames-a"] = $name;
+			$_SESSION["surname-a"] = $surname;
+			$_SESSION["firstname-a"] = $firstname;
+			$_SESSION["emails-a"] = $email;
+			
+			header("location: ../index.php");
+				
+		}
+		elseif ((mysqli_num_rows($result_name_c) || mysqli_num_rows($result_name_d)) == 1) {
+			
+			if (mysqli_num_rows($result_name_c) == 1) {
+				$_SESSION['message'] = "username exists";
 				$_SESSION["usernames-a"] = $name;
 				$_SESSION["surname-a"] = $surname;
 				$_SESSION["firstname-a"] = $firstname;
 				$_SESSION["emails-a"] = $email;
 				
 				header("location: ../index.php");
-				
+					
 			}
-			elseif ((mysqli_num_rows($result_name_c) || mysqli_num_rows($result_name_d)) == 1) {
-				
-				if (mysqli_num_rows($result_name_c) == 1) {
-					$_SESSION['message'] = "username exists";
-					$_SESSION["usernames-a"] = $name;
-					$_SESSION["surname-a"] = $surname;
-					$_SESSION["firstname-a"] = $firstname;
-					$_SESSION["emails-a"] = $email;
+			elseif (mysqli_num_rows($result_name_d) == 1) {
+				$_SESSION['message'] = "email exists";
+				$_SESSION["usernames-a"] = $name;
+				$_SESSION["surname-a"] = $surname;
+				$_SESSION["firstname-a"] = $firstname;
+				$_SESSION["emails-a"] = $email;
 					
-					header("location: ../index.php");
-					
-				}
-				elseif (mysqli_num_rows($result_name_d) == 1) {
-					$_SESSION['message'] = "email exists";
-					$_SESSION["usernames-a"] = $name;
-					$_SESSION["surname-a"] = $surname;
-					$_SESSION["firstname-a"] = $firstname;
-					$_SESSION["emails-a"] = $email;
-					
-					header("location: ../index.php");
-				}
+				header("location: ../index.php");
 			}
-			
+		}
+		elseif ((mysqli_num_rows($result_name_c) && mysqli_num_rows($result_name_d)) == 0) {
+
+			$conn->query("INSERT INTO tmanuel_users (usernames, passwords, emails, surname, firstname, middleName) VALUES ('$name',
+			'$passID', '$email', '$surname', '$firstname', '$middleName')") or die("unable to connect to MySQL at the moment ".$conn->connect_error);
+
+			unset ($_SESSION["usernames-a"]);
+			unset ($_SESSION["surname-a"]);
+			unset ($_SESSION["firstname-a"]);
+			unset ($_SESSION["emails-a"]);
+			unset ($_SESSION["message"]);
+
+			/*session_start();
+			$_SESSION["usernames"] = $name;
+			$_SESSION["passwords"] = $passID;
+			$_SESSION["emails"] = $email;*/
+
+			header("location: ../login.php");
+		}	
         }
         else {
 
